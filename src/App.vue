fc1adea0ab1647ffab4e9493fb7dc3885926698a
013-8 <00000000-0138-0139-0140-000000000000@proton.me>
2024-02-19 21:52:04 +0100

    made all text upper-case... Added booby-trap#0

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,9 @@ export default {
             this.soundBites[Math.floor(Math.random() * this.soundBites.length)].play()
             this.madnessLevel++
         },
+        resetMadness() {
+            this.madnessLevel = 0
+        },
         engageDemonMode() {
             this.isDemon = this.madnessLevel % 3 === 0
             return true
@@ -35,27 +38,28 @@ export default {
         </div>
         <div>
             <p id="description">
-                Lorem tempor aliqua do duis cupidatat ad sunt. Magna magna ex magna excepteur
-                officia ut in fugiat quis. Qui Lorem adipisicing incididunt magna ullamco et duis
-                cupidatat ipsum sint consequat nisi ea do. Laborum enim aliqua nisi duis eu fugiat
-                non sit ea incididunt magna. Ex exercitation fugiat commodo laborum occaecat ut
-                nulla mollit nisi pariatur fugiat et est. Non nostrud voluptate dolore dolore et.
-                Eiusmod consectetur aliquip et anim proident ullamco duis in qui cillum deserunt id
-                labore. Enim nostrud labore reprehenderit ipsum ut aliqua. Minim do aliqua ea et ex
-                aute amet eiusmod irure esse. Sint Lorem ex reprehenderit elit. Qui occaecat laboris
-                aliqua ipsum consectetur excepteur tempor eu non reprehenderit. Ut commodo amet
-                excepteur nisi esse velit. Commodo commodo velit minim pariatur excepteur ad mollit
-                sit dolor eiusmod tempor dolore enim. Lorem velit quis tempor laborum laborum cillum
-                adipisicing cillum labore commodo adipisicing. Deserunt irure incididunt aliqua
-                consectetur. Et incididunt cillum veniam pariatur. Sit aliquip pariatur magna est
-                occaecat proident deserunt quis ad. Amet officia velit sit eiusmod cillum velit
-                dolor nulla. Cillum eu commodo do esse ullamco ex deserunt dolore. Nostrud sit ad
-                sunt id ea ad reprehenderit. Officia do sint deserunt culpa do fugiat elit. Anim
-                eiusmod in quis elit exercitation magna velit minim. Fugiat anim aliquip occaecat
-                esse dolor est. Sint non ad commodo sunt mollit labore culpa consequat do pariatur.
-                Sunt aliquip anim sit minim {{ madnessLevel }} / 100 consequat ipsum tempor esse
-                minim mollit. Et anim sunt veniam culpa labore adipisicing incididunt id aliqua
-                ullamco aliquip Lorem dolor sint. Sit sunt deserunt aliqua pariatur esse.
+                LOREM TEMPOR ALIQUA DO DUIS CUPIDATAT AD SUNT. MAGNA MAGNA EX MAGNA EXCEPTEUR
+                OFFICIA UT IN FUGIAT QUIS. QUI LOREM ADIPISICING INCIDIDUNT MAGNA ULLAMCO ET DUIS
+                CUPIDATAT IPSUM SINT CONSEQUAT NISI EA DO. LABORUM ENIM ALIQUA NISI DUIS EU FUGIAT
+                NON SIT EA INCIDIDUNT MAGNA. EX EXERCITATION FUGIAT COMMODO LABORUM OCCAECAT UT
+                NULLA MOLLIT NISI PARIATUR FUGIAT ET EST. NON NOSTRUD VOLUPTATE DOLORE DOLORE ET.
+                EIUSMOD CONSECTETUR ALIQUIP ET ANIM PROIDENT ULLAMCO DUIS IN QUI CILLUM DESERUNT ID
+                LABORE. ENIM NOSTRUD LABORE REPREHENDERIT IPSUM UT ALIQUA. MINIM DO ALIQUA EA ET EX
+                AUTE AMET EIUSMOD IRURE ESSE. SINT LOREM EX REPREHENDERIT ELIT. QUI OCCAECAT LABORIS
+                ALIQUA IPSUM CONSECTETUR EXCEPTEUR TEMPOR EU NON REPREHENDERIT. UT COMMODO AMET
+                EXCEPTEUR NISI ESSE VELIT. COMMODO COMMODO VELIT MINIM PARIATUR EXCEPTEUR AD MOLLIT
+                SIT DOLOR EIUSMOD TEMPOR DOLORE ENIM. LOREM VELIT QUIS TEMPOR LABORUM LABORUM CILLUM
+                ADIPISICING CILLUM LABORE COMMODO ADIPISICING. DESERUNT IRURE INCIDIDUNT ALIQUA
+                CONSECTETUR. ET INCIDIDUNT CILLUM VENIAM PARIATUR. SIT ALIQUIP PARIATUR MAGNA EST
+                OCCAECAT PROIDENT DESERUNT QUIS AD. AMET OFFICIA VELIT SIT EIUSMOD CILLUM VELIT
+                DOLOR NULLA. CILLUM EU COMMODO DO ESSE ULLAMCO EX DESERUNT DOLORE. NOSTRUD SIT AD
+                SUNT ID EA AD REPREHENDERIT. OFFICIA DO SINT DESERUNT CULPA DO FUGIAT ELIT. ANIM
+                EIUSMOD IN QUIS ELIT EXERCITATION MAGNA VELIT MINIM. FUGIAT ANIM ALIQUIP OCCAECAT
+                ESSE DOLOR EST. SINT NON AD COMMODO SUNT MOLLIT LABORE CULPA CONSEQUAT DO PARIATUR.
+                SUNT ALIQUIP ANIM SIT MINIM
+                <span @click="resetMadness()">{{ madnessLevel }} / 100</span> CONSEQUAT IPSUM TEMPOR
+                ESSE MINIM MOLLIT. ET ANIM SUNT VENIAM CULPA LABORE ADIPISICING INCIDIDUNT ID ALIQUA
+                ULLAMCO ALIQUIP LOREM DOLOR SINT. SIT SUNT DESERUNT ALIQUA PARIATUR ESSE.
             </p>
         </div>
     </div>
@@ -91,6 +95,9 @@ p {
     margin-left: auto;
     margin-right: auto;
     opacity: 0.75;
-    padding: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 5px;
+    padding-bottom: 5px;
 }
 </style>
