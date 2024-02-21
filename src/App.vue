4b6c074fc7368285aabd5658f258e3e00dc67d7a
0138 <00000000-0138-0139-0140-000000000000@proton.me>
2024-02-21 20:37:50 +0100

    finished temp easter egg :) :) :)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ export default {
     data() {
         return {
             madnessLevel: 0,
+            maxMadness: 15,
             soundBites: [
                 new Audio('/sound_bites/mad.mp3'),
                 new Audio('/sound_bites/madness.mp3'),
@@ -11,33 +12,93 @@ export default {
                 new Audio('/sound_bites/this_is_lillevannet.mp3'),
                 new Audio('/sound_bites/this_is_madness.mp3')
             ],
-            isDemon: false
+            secretSoundBytes: [
+                new Audio('/sound_bites/this_shit_aint_nothing_to_me_man.mp3'),
+                new Audio('/sound_bites/im_really_him.mp3')
+            ]
         }
     },
     methods: {
         playAudio() {
-            if (!this.soundBites) return
-            this.soundBites[Math.floor(Math.random() * this.soundBites.length)].play()
+            if (!this.soundBites || !this.secretSoundBytes) return
             this.madnessLevel++
+            if (this.madnessLevel % 5 === 0) {
+                this.secretSoundBytes[0].play()
+                return
+            }
+            this.soundBites[Math.floor(Math.random() * this.soundBites.length)].play()
         },
         resetMadness() {
             this.madnessLevel = 0
         },
-        engageDemonMode() {
-            this.isDemon = this.madnessLevel % 3 === 0
-            return true
+        isDemon() {
+            if (this.madnessLevel > this.maxMadness) this.resetMadness()
+            if (this.madnessLevel >= this.maxMadness) {
+                this.secretSoundBytes[1].play()
+                return true
+            }
+            return false
         }
     }
 }
 </script>
 
 <template>
-    <div id="wrapper">
-        <div @click.prevent="playAudio()">
-            <h1>V I K A P E D I A</h1>
+    <div v-if="isDemon()" class="wrapper demon-mode">
+        <div @click.prevent="playAudio()" class="demon-mode">
+            <h1>💀 V I K A P E D I A 💀</h1>
         </div>
         <div>
-            <p id="description">
+            <p class="description demon-mode">
+                NULLA UT ULLAMCO CULPA EST ET OFFICIA ULLAMCO LABORUM PROIDENT QUIS COMMODO
+                CUPIDATAT NISI. ENIM SINT CONSECTETUR EXERCITATION EA. ALIQUA ANIM DO LOREM DUIS.
+                EIUSMOD EXERCITATION LABORIS NOSTRUD LABORE DOLOR. EST ANIM PROIDENT LABORE DESERUNT
+                QUIS PROIDENT. ALIQUIP IN EU DO AD DOLORE EX VOLUPTATE SIT. COMMODO AD EXERCITATION
+                ULLAMCO ALIQUA DOLORE EST IRURE PROIDENT LOREM EX SINT LABORUM. UT TEMPOR IN
+                REPREHENDERIT OCCAECAT IRURE COMMODO NISI COMMODO MINIM UT AMET. VELIT AD ULLAMCO
+                IRURE ANIM ID OFFICIA FUGIAT. AD TEMPOR ALIQUA QUI LABORIS IPSUM MAGNA. EA ET EX
+                AUTE TEMPOR CONSEQUAT MINIM NOSTRUD CULPA. LABORUM NON CONSECTETUR COMMODO DOLOR
+                CONSEQUAT IPSUM LABORUM. EA EXCEPTEUR ESSE CONSEQUAT DESERUNT DESERUNT AMET EU DO.
+                INCIDIDUNT COMMODO CONSEQUAT ULLAMCO NISI EXERCITATION LOREM ADIPISICING NULLA.
+                DOLOR DUIS EIUSMOD OFFICIA LABORE LABORE EXCEPTEUR IPSUM. EA NULLA NISI IRURE DOLOR
+                CILLUM MINIM INCIDIDUNT SUNT ANIM DESERUNT SUNT COMMODO. OCCAECAT IRURE CONSECTETUR
+                SUNT EXCEPTEUR. SIT LABORUM EXERCITATION PARIATUR REPREHENDERIT CUPIDATAT TEMPOR
+                PROIDENT COMMODO ELIT. OFFICIA QUIS ENIM DOLORE EXCEPTEUR QUIS LOREM ALIQUA EA ESSE
+                AUTE. NON ALIQUIP TEMPOR IN EXERCITATION COMMODO LABORIS ET FUGIAT EX SUNT CUPIDATAT
+                ELIT. OFFICIA CUPIDATAT QUI QUI ENIM ENIM ESSE PARIATUR LOREM QUIS LABORIS PROIDENT
+                DOLORE IN. ENIM NULLA FUGIAT ALIQUA ALIQUIP MOLLIT DUIS OCCAECAT ESSE LABORE
+                CUPIDATAT. MOLLIT SIT IPSUM ADIPISICING IN ELIT VENIAM. DO DO OCCAECAT NULLA
+                PROIDENT EU SUNT VENIAM. CILLUM ESSE ANIM PARIATUR CULPA NULLA VENIAM DESERUNT. UT
+                DO TEMPOR ESSE AUTE. ELIT UT AMET VOLUPTATE MINIM LABORUM MAGNA QUI QUI INCIDIDUNT
+                LABORIS PROIDENT AD. ULLAMCO PROIDENT LABORIS ULLAMCO INCIDIDUNT LABORUM AMET
+                CONSECTETUR IPSUM LABORUM. DOLORE EXERCITATION CONSECTETUR ID COMMODO OFFICIA IPSUM
+                EA AUTE CONSECTETUR CUPIDATAT MINIM CONSECTETUR EXCEPTEUR ENIM. EIUSMOD VOLUPTATE
+                OCCAECAT NOSTRUD LOREM MOLLIT. INCIDIDUNT ALIQUA QUI SUNT DOLORE QUIS MOLLIT ELIT
+                IRURE LABORUM COMMODO AMET EXERCITATION. SINT INCIDIDUNT MINIM VELIT AD DUIS MAGNA
+                CULPA DOLOR FUGIAT UT MAGNA ULLAMCO OFFICIA CONSECTETUR. OCCAECAT PARIATUR EA MOLLIT
+                OFFICIA CULPA ADIPISICING. MINIM DOLORE FUGIAT EXCEPTEUR AUTE CULPA ULLAMCO EA UT
+                ALIQUA CILLUM MAGNA DUIS DOLOR DESERUNT. ADIPISICING OFFICIA NON SIT REPREHENDERIT
+                NULLA LABORUM ANIM ENIM FUGIAT CONSECTETUR NOSTRUD EIUSMOD. IN EIUSMOD CONSEQUAT
+                SINT MOLLIT EXERCITATION UT REPREHENDERIT NOSTRUD MAGNA. AUTE NULLA OFFICIA VELIT
+                QUIS ET ALIQUA EXCEPTEUR CILLUM OFFICIA EXCEPTEUR LABORE MOLLIT VENIAM. QUIS MAGNA
+                LABORUM ELIT AD VENIAM VENIAM NOSTRUD EXCEPTEUR. IPSUM IN INCIDIDUNT EA LOREM LABORE
+                ET. ELIT ULLAMCO ULLAMCO LABORUM SIT DOLORE CILLUM SUNT ET DESERUNT ALIQUIP. EU QUIS
+                MAGNA VENIAM MAGNA PROIDENT DO FUGIAT NULLA SIT PARIATUR ESSE DOLORE NON CUPIDATAT.
+                ENIM MOLLIT ELIT LABORE MOLLIT CONSEQUAT DOLOR. DOLORE PROIDENT ENIM ALIQUIP DUIS.
+                ALIQUA EXERCITATION QUIS MINIM REPREHENDERIT AD VOLUPTATE AMET NOSTRUD. PROIDENT QUI
+                TEMPOR ALIQUIP QUI IN EU CILLUM QUIS. IN CONSECTETUR DOLOR NOSTRUD ESSE LABORE
+                INCIDIDUNT AMET. AD LABORE SINT OFFICIA QUI ESSE TEMPOR NISI OFFICIA UT DOLOR
+                REPREHENDERIT AD ALIQUIP. NULLA ET NISI VOLUPTATE EIUSMOD IN NON SIT EST SUNT CULPA
+                VENIAM CULPA.
+            </p>
+        </div>
+    </div>
+    <div v-else class="wrapper">
+        <div @click.prevent="playAudio()">
+            <h1>&zwnj; V I K A P E D I A &zwnj;</h1>
+        </div>
+        <div>
+            <p class="description">
                 LOREM TEMPOR ALIQUA DO DUIS CUPIDATAT AD SUNT. MAGNA MAGNA EX MAGNA EXCEPTEUR
                 OFFICIA UT IN FUGIAT QUIS. QUI LOREM ADIPISICING INCIDIDUNT MAGNA ULLAMCO ET DUIS
                 CUPIDATAT IPSUM SINT CONSEQUAT NISI EA DO. LABORUM ENIM ALIQUA NISI DUIS EU FUGIAT
@@ -57,16 +118,21 @@ export default {
                 EIUSMOD IN QUIS ELIT EXERCITATION MAGNA VELIT MINIM. FUGIAT ANIM ALIQUIP OCCAECAT
                 ESSE DOLOR EST. SINT NON AD COMMODO SUNT MOLLIT LABORE CULPA CONSEQUAT DO PARIATUR.
                 SUNT ALIQUIP ANIM SIT MINIM
-                <span @click="resetMadness()">{{ madnessLevel }} / 100</span> CONSEQUAT IPSUM TEMPOR
-                ESSE MINIM MOLLIT. ET ANIM SUNT VENIAM CULPA LABORE ADIPISICING INCIDIDUNT ID ALIQUA
-                ULLAMCO ALIQUIP LOREM DOLOR SINT. SIT SUNT DESERUNT ALIQUA PARIATUR ESSE.
+                <span @click="resetMadness()">{{ madnessLevel }} / {{ maxMadness }}</span> CONSEQUAT
+                IPSUM TEMPOR ESSE MINIM MOLLIT. ET ANIM SUNT VENIAM CULPA LABORE ADIPISICING
+                INCIDIDUNT ID ALIQUA ULLAMCO ALIQUIP LOREM DOLOR SINT. SIT SUNT DESERUNT ALIQUA
+                PARIATUR ESSE.
             </p>
         </div>
     </div>
 </template>
 
 <style>
-#wrapper {
+.demon-mode {
+    color: RGB(190, 75, 219) !important;
+    -webkit-text-stroke: black;
+}
+.wrapper {
     user-select: none;
     color: #f0f7fd;
     width: 100%;
