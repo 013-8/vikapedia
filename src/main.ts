0fb7f08c04c532fba943950df5d985ca5d3617fe
0138 <00000000-0138-0139-0140-000000000000@proton.me>
2024-02-21 09:43:45 +0100

    fresh slate

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import firebase from 'firebase/app'
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import { firebaseConfig } from '../firebase-config'
+// import firebase from 'firebase/app'
+// import { initializeApp } from 'firebase/app'
+// import { getAnalytics } from 'firebase/analytics'
+// import { firebaseConfig } from '../firebase-config'
 
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-// const app = createApp(App)
-// app.use(createPinia())
-// app.use(router)
-// app.mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-export default firebase
+// const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
+// export default firebase
